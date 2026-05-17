10c6170aa99f7fdfdb3512377572df0c596a6ebe
martinggg11g-hash <martinggg11g@gmail.com>
2026-05-17 04:22:42 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,10 @@ export default defineConfig({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
       workbox: {
-        // Le damos a Vercel la configuración de runtimeCaching que estaba pidiendo a gritos
+        // 👉 ACÁ ESTIRAMOS EL LÍMITE A 10 MB
+        maximumFileSizeToCacheInBytes: 10485760,
+        // Y le dejamos a Vercel las reglas claras para que no se queje
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,lottie,json}'],
         runtimeCaching: [
           {
             urlPattern: ({ request }) => request.destination === 'image' || request.destination === 'document' || request.destination === 'script',
