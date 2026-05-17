d6eb322e0bf6a9b166f7bc596ca7c2f6719642ea
martinggg11g-hash <martinggg11g@gmail.com>
2026-05-17 04:20:34 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        // Le damos a Vercel la configuración de runtimeCaching que estaba pidiendo a gritos
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'image' || request.destination === 'document' || request.destination === 'script',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'fiesta-app-cache',
+            }
+          }
+        ]
+      }
+    })
   ],
 })
