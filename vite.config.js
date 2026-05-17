561d84ca7ca98c86aa8358412faf9e20e244f1b5
martinggg11g-hash <martinggg11g@gmail.com>
2026-05-17 04:15:30 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: false,
-      // 👉 ACÁ ESTÁ EL SALVAVIDAS PARA VERCEL
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2,json,lottie}'],
-        maximumFileSizeToCacheInBytes: 8000000 // Subimos el límite a 8MB por las dudas
-      }
-    })
+    react()
   ],
 })
